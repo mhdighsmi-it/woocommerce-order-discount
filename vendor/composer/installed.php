857da4d2ff23d77bacf4979564bae22a2bac9641
mhdighsmi <mhdighsmi.it@gmail.com>
2021-08-25 22:04:34 +0430

    create-namespase-plugin-with-composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '392eaa20afd47222bbdad89291bdc3efc33d5b5f',
+    'name' => 'mehdighasemi/discount-woocommerce',
+  ),
+  'versions' => 
+  array (
+    'mehdighasemi/discount-woocommerce' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '392eaa20afd47222bbdad89291bdc3efc33d5b5f',
+    ),
+  ),
+);
